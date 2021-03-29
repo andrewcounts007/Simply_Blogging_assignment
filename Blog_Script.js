41d903a61dcef76a2e7272ba3b8c58e42e37bc12
andrew counts <andrewcounts007@gmail.com>
2021-03-28 20:12:23 -0500

    some basic componants added

---
+++ Blog_Script.js
@@ -0,0 +1,12 @@
+//function post(){ 
+//var blog = document.getElementById("blogbox");
+//document.write(blog);
+//document.getElementById('blogbox').value = "Write your cool new blog here..."
+//}
+
+function post(){
+    var blog = document.getElementById("blogbox").value;
+    document.write(blog);
+    
+}
+
